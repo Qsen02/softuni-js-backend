9785b5d1a55d2949792f5f691f5a4c2be4eddfcf
Qsen02 <diyangenchev73@gmail.com>
2024-06-07 15:36:41 +0300

    Update mongoose.js

--- Session and Authentication/Movie Magic - Part 3/src/config/mongoose.js
+++ Session and Authentication/Movie Magic - Part 3/src/config/mongoose.js
@@ -1,4 +1,7 @@
 let mongoose = require("mongoose");
+const { Movies } = require("../models/movies");
+const { Users } = require("../models/users");
+const { Casts } = require("../models/casts");
 
 async function main() {
     await mongoose.connect("mongodb://localhost:27017/moviesdb");
