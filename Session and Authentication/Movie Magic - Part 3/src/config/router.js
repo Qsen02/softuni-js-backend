6cb183a11b44c7621aa108a697e2cdf0ae586b0a
Qsen02 <diyangenchev73@gmail.com>
2024-06-07 15:30:21 +0300

    guards added

--- Session and Authentication/Movie Magic - Part 3/src/config/router.js
+++ Session and Authentication/Movie Magic - Part 3/src/config/router.js
@@ -9,33 +9,34 @@ const { onDelete, showDeleteForm, onRefuse } = require("../controllers/deleteMov
 const { showRegisterForm, onRegister } = require("../controllers/register");
 const { showLoginForm, onLogin } = require("../controllers/login");
 const { showEditForm, onEdit } = require("../controllers/edit");
+const { isGuest, isUser } = require("../middlewears/guards");
 
 let router = Router();
 
 router.get("/", showHome);
 router.get("/about", showAbout);
 router.get("/details/:id", showDetails);
-router.get("/create", showCreateForm);
-router.post("/create", onCreate);
+router.get("/create", isUser(), showCreateForm);
+router.post("/create", isUser(), onCreate);
 router.get("/searcher", showSerchMenu);
 router.get("/search?*", onSearch);
-router.get("/cast/create", showCastForm);
-router.post("/cast/create", onCreateCast);
-router.get("/movies/:id/attach", showAttachForm);
-router.post("/movies/:id/attach", onAttach);
-router.get("/movies/:id/delete", showDeleteForm);
-router.get("/movies/:id/delete/yes", onDelete);
-router.get("/movies/:id/delete/no", onRefuse);
-router.get("/register", showRegisterForm);
-router.post("/register", onRegister);
-router.get("/movies/:id/edit", showEditForm);
-router.post("/movies/:id/edit", onEdit);
+router.get("/cast/create", isUser(), showCastForm);
+router.post("/cast/create", isUser(), onCreateCast);
+router.get("/movies/:id/attach", isUser(), showAttachForm);
+router.post("/movies/:id/attach", isUser(), onAttach);
+router.get("/movies/:id/delete", isUser(), showDeleteForm);
+router.get("/movies/:id/delete/yes", isUser(), onDelete);
+router.get("/movies/:id/delete/no", isUser(), onRefuse);
+router.get("/register", isGuest(), showRegisterForm);
+router.post("/register", isGuest(), onRegister);
+router.get("/movies/:id/edit", isUser(), showEditForm);
+router.post("/movies/:id/edit", isUser(), onEdit);
 router.get("/logout", (req, res) => {
     res.clearCookie("token");
     res.redirect("/");
 });
-router.get("/login", showLoginForm);
-router.post("/login", onLogin);
+router.get("/login", isGuest(), showLoginForm);
+router.post("/login", isGuest(), onLogin);
 router.get("*", (req, res) => {
     res.render("error");
 })
